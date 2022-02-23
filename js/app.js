8fd549ad85e83406592a43f19762e893501ef932
Racknar0 <racknarow1@gmail.com>
2022-02-23 18:49:13 -0500

    Add Validation

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Variables y Selectores
+const formulario = document.querySelector('#agregar-gasto')
+const gastoListado = document.querySelector('#gastos ul')
+
+// Eventos
+
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+}
+
+
+// Clases
+
+
+
+// Funciones
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('Cual es tu presupuesto', 0);
+    
+    
+    if ( presupuestoUsuario === '' ||  presupuestoUsuario === null || isNaN(presupuestoUsuario) ||  presupuestoUsuario <= 0 ) {
+        window.location.reload();
+    } 
+}
+
